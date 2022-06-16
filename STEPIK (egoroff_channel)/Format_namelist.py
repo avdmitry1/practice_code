495254959ed80078e1e4a6c8ca77a24fcbc87dbc
avdmitry <avdmitry1@gmail.com>
2022-06-16 17:38:42 +0300

    New file

--- STEPIK (egoroff_channel)/Format_namelist.py
+++ STEPIK (egoroff_channel)/Format_namelist.py
@@ -1,5 +1,4 @@
 def format_namelist(name):
-    result = ''
     names = [i['name'] for i in name]
     if len(names) == 0:
         return ''
@@ -9,8 +8,7 @@ def format_namelist(name):
         return ' и '.join(names)
     elif len(names) >= 3:
         for i in names[:-2]:
-            result += i + ', '.join(names[:-2])
-        return result + names[-2] + ' и ' + names[-1]
+            return ', '.join(names[:-1]) + ' и ' + names[-1]
 
 
-print(format_namelist([ {'name': 'Bart'}, {'name': 'Lisa'}]))
+print(format_namelist([ {'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'} ]))
