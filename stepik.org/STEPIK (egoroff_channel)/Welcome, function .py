16043b0a0e0bcf5200466933db6fe334b2a82e2a
avdmitry <avdmitry1@gmail.com>
2022-06-18 14:08:24 +0300

    New file

---
+++ stepik.org/STEPIK (egoroff_channel)/Welcome, function .py
@@ -0,0 +1,28 @@
+def greet(language):
+    x = {
+        'english': 'Welcome',
+        'czech': 'Vitejte',
+        'danish': 'Velkomst',
+        'dutch': 'Welkom',
+        'estonian': 'Tere tulemast',
+        'finnish': 'Tervetuloa',
+        'flemish': 'Welgekomen',
+        'french': 'Bienvenue',
+        'german': 'Willkommen',
+        'irish': 'Failte',
+        'italian': 'Benvenuto',
+        'latvian': 'Gaidits',
+        'lithuanian': 'Laukiamas',
+        'polish': 'Witamy',
+        'spanish': 'Bienvenido',
+        'swedish': 'Valkommen',
+        'welsh': 'Croeso'
+    }
+    for key, value in x.items():
+        if str(language) in key:
+            return value
+    else:
+        return 'Welcome'
+
+
+print(greet('dutch'))
