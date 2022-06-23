0c0d20e4ca50cd6feb6d171040e2be570f64a4d9
avdmitry <avdmitry1@gmail.com>
2022-06-23 17:01:26 +0300

    New file

---
+++ codewars.com/7kyu/#26.py
@@ -0,0 +1,8 @@
+def disemvowel(string_):
+    return string_.replace('a', '').replace('e', '').replace('i', '').replace('o', '').replace('u', '')\
+        .replace('A', '').replace('E', '').replace('I', '').replace('O', '').replace('U', '')
+
+
+print(disemvowel("This website is for losers LOL!"))
+
+
