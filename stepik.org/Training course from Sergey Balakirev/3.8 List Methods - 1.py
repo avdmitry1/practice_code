7dbadcc7deb5f22468036ed3687600f66bfce196
avdmitry <avdmitry1@gmail.com>
2022-07-13 12:40:16 +0300

    New file

--- stepik.org/Training course from Sergey Balakirev/3.8 List Methods - 1.py
+++ stepik.org/Training course from Sergey Balakirev/3.8 List Methods - 1.py
@@ -1,3 +1,6 @@
+# A string of integers separated by a space is entered. If the first number is not equal to the last, then you need to
+# add the True value, otherwise, the False value. Display the resulting list
+
 a_list = list(map(int, input().split()))
 
 a_list.append(a_list[0] != a_list[-1])
