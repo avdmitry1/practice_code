b1da28cdf0d3017f0d08831bf3ff0365fe67fb50
avdmitry <avdmitry1@gmail.com>
2022-07-15 00:02:48 +0300

    New file

--- stepik.org/Training course from Sergey Balakirev/4.2 Nested Conditions and Multiple Choices 1.py
+++ stepik.org/Training course from Sergey Balakirev/4.2 Nested Conditions and Multiple Choices 1.py
@@ -1,5 +1,8 @@
 import datetime
+
 month, date = map(int, input().split())
 
 day = datetime.date(2021, month, date)
-print(day)
+yesterday = datetime.timedelta(days=+1)
+tomorrow = datetime.timedelta(days=-1)
+print(((day - yesterday).strftime("%m.%d")), ((day - tomorrow).strftime("%m.%d")))
