ef5b8053d7576bfef22b1aa2b6935b44ed191a13
avdmitry <avdmitry1@gmail.com>
2022-07-22 17:50:26 +0300

    New file

---
+++ codewars.com/8kyu/#106.py
@@ -0,0 +1,9 @@
+# Write a function which calculates the average of the numbers in a given list.
+#
+# Note: Empty arrays should return 0.
+
+def find_average(numbers):
+    return sum(numbers) // len(numbers)
+
+
+print(find_average([1, 2, 3]))
