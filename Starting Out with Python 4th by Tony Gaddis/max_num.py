2dbe2e52e318613d52bbc44eb8e8c9bd699c09f1
avdmitry1 <avdmitry1@gmail.com>
2023-02-14 10:41:05 +0200

    new file

---
+++ Starting Out with Python 4th by Tony Gaddis/max_num.py
@@ -0,0 +1,8 @@
+def max_num(num1, num2):
+    return max(num1, num2)
+
+
+number1 = int(input('Enter first num: '))
+number2 = int(input('Enter second num: '))
+
+print(f'Max number is {max_num(number1, number2)}')
