d62f5556a51c64ff626e07cb2731ec982f3ffe37
avdmitry1 <avdmitry1@gmail.com>
2023-03-06 12:12:50 +0200

    new file

---
+++ Starting Out with Python 4th by Tony Gaddis/search_coffee.record.py
@@ -0,0 +1,30 @@
+# Программа по поиску записей файла coffee.txt
+def main():
+    # Создаем булевую переменную в качестве флага
+    found = False
+    search = input('Получить искомое описание: ')
+    # Открыть файл coffee.txt
+    coffee_file = open('coffee.txt', 'r')
+    # Прочитать после с описанием кофе первой записи
+    descr = coffee_file.readline().rstrip('\n')
+    # Прочитать остаток файла
+    while descr != '':
+        qty = float(coffee_file.readline())
+        # Определить соответствует ли запись поиску
+        if descr == search:
+            # Показать запись
+            print(f'Описание: {descr}')
+            print(f'Количество: {qty}')
+            print()
+            # Назначит флагу значение True
+            found = True
+        # Прочитать следующее описание
+        descr = coffee_file.readline()
+    coffee_file.close()
+    # Если значение не найдено показать сообщение
+    if not found:
+        print('Это значение в файле не найдено')
+
+
+if __name__ == '__main__':
+    main()
