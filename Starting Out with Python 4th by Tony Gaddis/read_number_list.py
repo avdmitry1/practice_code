7e4b31649efb0d11cc98ce911c7b9c0e157471ea
avdmitry1 <avdmitry1@gmail.com>
2023-03-07 13:14:03 +0200

    new file

--- Starting Out with Python 4th by Tony Gaddis/read_number_list.py
+++ Starting Out with Python 4th by Tony Gaddis/read_number_list.py
@@ -1,7 +1,8 @@
 def main():
     with open('number_list.txt', 'r') as file_nums:
-        nums = file_nums.read()
-        print(nums)
+        nums = file_nums.readlines()
+        num_list = [int(i.rstrip('\n')) for i in nums]
+        print(num_list)
 
 
 if __name__ == '__main__':
