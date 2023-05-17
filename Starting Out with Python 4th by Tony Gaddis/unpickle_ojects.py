86f1403b415ac12741d51a781448f68961131b0d
avdmitry1 <avdmitry1@gmail.com>
2023-05-17 12:31:35 +0300

    new file

---
+++ Starting Out with Python 4th by Tony Gaddis/unpickle_ojects.py
@@ -0,0 +1,21 @@
+import pickle
+
+def main():
+    end_of_file = False
+    input_file = open('info.dat', 'rb')
+    while not end_of_file:
+        try:
+            person = pickle.load(input_file)
+            display_data(person)
+        except EOFError:
+            end_of_file = True
+    input_file.close()
+    
+def display_data(person):
+    print('Имя: ', person['Имя'])
+    print('Возраст: ', person['Возраст'])
+    print('Масса: ', person['Масса'])
+    print()
+    
+if __name__ == '__main__':
+    main()
