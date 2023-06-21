097cc12e5cdd9eb538008100868154b3613dc1fc
avdmitry1 <avdmitry1@gmail.com>
2023-06-21 22:54:29 +0300

    new file

--- Starting Out with Python 4th by Tony Gaddis/vehicles.py
+++ Starting Out with Python 4th by Tony Gaddis/vehicles.py
@@ -24,3 +24,14 @@ class Automobile:
         return self.__mileage
     def get_price(self):
         return self.__price
+    
+    
+class Car(Automobile):
+    def __init__(self, make, model, mileage, price, doors):
+        Automobile.__init__(self, make, model, mileage, price)
+        self.__doors = doors
+        
+    def set_doors(self, doors):
+        self.__doors = doors
+    def get_doors(self):
+        return self.__doors
