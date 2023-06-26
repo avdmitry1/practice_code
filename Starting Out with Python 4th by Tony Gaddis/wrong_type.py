5d780aa098d34ed028f0cd7b6c8428215c43aee6
avdmitry1 <avdmitry1@gmail.com>
2023-06-26 21:47:16 +0300

    new file

---
+++ Starting Out with Python 4th by Tony Gaddis/wrong_type.py
@@ -0,0 +1,11 @@
+def main():
+    show_mammal_info('Я - последовательность символов')
+    
+
+def show_mammal_info(creature):
+    creature.show_species()
+    creature.make_sound()
+    
+    
+if __name__ == '__main__':
+    main()
