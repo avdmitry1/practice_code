d1f5353f9e430fd9e82b89cf818187f81e114c64
avdmitry1 <avdmitry1@gmail.com>
2024-02-14 20:28:50 +0200

    new file

--- stepik.org/Training course from Sergey Balakirev/5.6 Nested Loops 2.py
+++ stepik.org/Training course from Sergey Balakirev/5.6 Nested Loops 2.py
@@ -1,12 +1,12 @@
 # A natural number n is entered. It is necessary to find all prime numbers that are less than this number n, that is,
 # in the range [2; n). Output the result to the screen in a line separated by a space.
 
-n = int(input())
-a_list = []
-for i in range(2, n):
-    for j in range(2, i):
-        if i % j == 0:
-            break
-    else:
-        a_list.append(i)
-print(*a_list)
+# n = int(input())
+# a_list = []
+# for i in range(2, n):
+#     for j in range(2, i):
+#         if i % j == 0:
+#             break
+#     else:
+#         a_list.append(i)
+# print(*a_list)
