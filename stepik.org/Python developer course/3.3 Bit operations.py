a00b55edf61552bf38b37b27bbeda2537e44f19b
avdmitry1 <avdmitry1@gmail.com>
2024-03-19 17:16:48 +0200

    new file

--- stepik.org/Python developer course/3.3 Bit operations.py
+++ stepik.org/Python developer course/3.3 Bit operations.py
@@ -1,11 +1,11 @@
-# a = 17
-# print(bin(a))
-# print(oct(a))
-# print(hex(a))
+a = 17
+print(bin(a))
+print(oct(a))
+print(hex(a))
 
-# number = input()
-# decimal_number = int(number, 6)
-# print(hex(decimal_number))
+number = input()
+decimal_number = int(number, 6)
+print(hex(decimal_number))
 
 
 
