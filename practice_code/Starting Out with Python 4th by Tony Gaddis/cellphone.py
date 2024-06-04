8f8ca1a8a9b0fee21d825e2938ad9aa25ed9aca6
Avramenko Dmitry <avdmitry1@gmail.com>
2024-06-04 18:00:55 +0300

    s

--- practice_code/Starting Out with Python 4th by Tony Gaddis/cellphone.py
+++ practice_code/Starting Out with Python 4th by Tony Gaddis/cellphone.py
@@ -1,23 +1,23 @@
-#  Класс cellphone содерожит данные о сотовом телефоне
+# #  Класс cellphone содерожит данные о сотовом телефоне
 
-class CellPhone:
-    #  Метод __init__ инициализирует атрибуты
-    def __init__(self, manufact, model, price):
-        self.__manufact = manufact
-        self.__model = model
-        self.__price = price
-    #  Методы manufact, model, price принимают аргументы
-    def set_manufact(self, manufact):
-        self.__manufact = manufact
-    def set_model(self, model):
-        self.__model = model
-    def set_retail_price(self, price):
-        self.__price = price
-    #  Методы manufact, model, price принимают аргументы
-    def get_manufact(self):
-        return self.__manufact
-    def get_model(self):
-        return self.__model
-    def get_retail_price(self):
-        return self.__price
+# class CellPhone:
+#     #  Метод __init__ инициализирует атрибуты
+#     def __init__(self, manufact, model, price):
+#         self.__manufact = manufact
+#         self.__model = model
+#         self.__price = price
+#     #  Методы manufact, model, price принимают аргументы
+#     def set_manufact(self, manufact):
+#         self.__manufact = manufact
+#     def set_model(self, model):
+#         self.__model = model
+#     def set_retail_price(self, price):
+#         self.__price = price
+#     #  Методы manufact, model, price принимают аргументы
+#     def get_manufact(self):
+#         return self.__manufact
+#     def get_model(self):
+#         return self.__model
+#     def get_retail_price(self):
+#         return self.__price
         
