4a779603452c79382c100cb673663881bc1b0b75
avdmitry <avdmitry1@gmail.com>
2024-09-21 09:34:00 +0300

    new file

--- CyberBionic/iter/dict_task.py
+++ CyberBionic/iter/dict_task.py
@@ -1,24 +1,25 @@
 products = {
     "Електроніка": ["Телефон", "Ноутбук", "Навушники"],
     "Одяг": ["Футболка", "Джинси", "Куртка"],
-    "Книги": ["Роман", "Фентезі", "Наукова література"]
+    "Книги": ["Роман", "Фентезі", "Наукова література"],
 }
+
 
 class MyIterator:
     def __init__(self, dictionary):
         self.items = list(dictionary.items())
         self.index = 0
-        
+
     def __iter__(self):
         return self
-    
+
     def __next__(self):
         if self.index < len(self.items):
             result = self.items[self.index]
             self.index += 1
             return result
-        else:
-            raise StopIteration
+        raise StopIteration
+
 
 for key, values in MyIterator(products):
     for value in values:
