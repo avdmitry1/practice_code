8f8ea994cbd672b8a0857809a724d61375df3f15
avdmitry <avdmitry1@gmail.com>
2024-09-23 17:59:53 +0300

    new file

--- CyberBionic/generator_/task3.py
+++ CyberBionic/generator_/task3.py
@@ -8,5 +8,5 @@ def is_simple_num_generator(lst: list):
         else:
             yield i
 nums_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-is_simple = list(x for x in is_simple_num_generator(nums_list))
-print(is_simple)
+is_simple_num = list(x for x in is_simple_num_generator(nums_list))
+print(is_simple_num)
